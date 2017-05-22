326ddcce54def8c0d3ed5405fcbd7723d50fb1ca
Colin Hicks <colin.hicks@gmail.com>
2017-05-22 12:16:12 -0400

    ES2015-based client

---
+++ test/index.js
@@ -0,0 +1,81 @@
+import expect from 'expect.js';
+import PyroclastClient from '../src/pyroclast';
+
+describe('PyroclastClient', function() {
+    const writeApiKey = 'key';
+    const endpoint = 'http://noop';
+    
+    it('fails to construct when required options are not specified.', function() {
+        expect(() => new PyroclastClient({})).to.throwError();
+    });
+    
+    it('bootstraps a default fetch impl under Node', function() {
+        const c = new PyroclastClient({writeApiKey, endpoint});
+        expect(c.fetchImpl).to.be.ok();
+    });
+
+    const mockFetch = (url, {body}) => {
+        body = JSON.parse(body);
+        return new Promise((resolve, reject) => {
+            switch (body.mocking) {
+            case 200:
+                return resolve({
+                    status: 200,
+                    json: () => { return {created: true};}
+                });
+            case 400:
+            case 401:
+                return resolve({
+                    status: body.mocking
+                });
+            case 999:
+                return resolve({
+                    status: body.mocking,
+                    responseText: 'potato'
+                });
+            default:
+                return reject(new Error(body));
+            }});
+    };
+
+    mockFetch.custom = true;
+
+    it('uses user-specified fetch implementation when provided.', function() {
+        const c = new PyroclastClient({writeApiKey, endpoint, fetchImpl: mockFetch});
+        expect(c.fetchImpl.custom).to.be.ok();
+    });
+
+    it('should promise parsed json from successful requests', function(done) {
+        const c = new PyroclastClient({writeApiKey, endpoint, fetchImpl: mockFetch});
+        c.sendEvent('atopic', {foo: 'bar', mocking: 200})
+            .then((result) => {
+                expect(result.created).to.be.ok();
+                done();
+            })
+            .catch(done);
+    });
+
+    it('should reject on 400s', function(done) {
+        const c = new PyroclastClient({writeApiKey, endpoint, fetchImpl: mockFetch});
+        c.sendEvent('atopic', {foo: 'bar', mocking: 400})
+            .then((result) => {
+                done('Not expected');
+            })
+            .catch((err) => {
+                expect(err).to.be.an(Error);
+                done();
+            });
+    });
+
+    it('should reject on network error', function(done) {
+        const c = new PyroclastClient({writeApiKey, endpoint, fetchImpl: mockFetch});
+        c.sendEvent('atopic', {foo: 'bar', mocking: null})
+            .then((result) => {
+                done('Not expected');
+            })
+            .catch((err) => {
+                expect(err).to.be.an(Error);
+                done();
+            });
+    })
+});
